7a7c8b10a248a9070873caff2d8acf831aa8ae05
Dewald Viljoen <79870549+Dev-ops-true@users.noreply.github.com>
2021-05-04 16:32:19 +0100

    after finishing step 7

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,33 @@
-describe Ostercard do
-    
+require 'oystercard'
+
+describe Oystercard do
+
+  it 'Oystercards that have been initialized have a balance of 0' do
+    expect(subject.balance).to eq(0)
+   end
+
+  describe '#top_up' do
+
+    it { is_expected.to respond_to(:top_up).with(1).argument }
+  
+    it 'Oystercard will top up the balance with the amount in argument' do
+      expect{ subject.top_up 10 }.to change{ subject.balance }.by 10
+    end
+
+    it 'does not allow the balance to be more than £90' do
+        maximum_balance = Oystercard::MAXIMUM_BALANCE
+        subject.top_up maximum_balance
+        expect { subject.top_up 1 }.to raise_error "You cannot exceed #{maximum_balance}"
+    end
+
+  end
+  describe '#deduct' do
+
+    it { is_expected.to respond_to(:deduct).with(1).argument }
+
+    it 'deducts the fare from my balance' do
+    expect{subject.deduct 10}.to change{ subject.balance }.by(-10)
+    endß
+  end
+
 end
